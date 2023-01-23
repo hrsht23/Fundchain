72ece2b92826804e713856626d5d0df7304a5969
Yogesh Singh Nayal <ysnayal17@student.sfit.ac.in>
2023-01-23 07:51:45 +0000

    Create Pool Working :)

--- ethereum/discountmain.js
+++ ethereum/discountmain.js
@@ -3,7 +3,7 @@ import web3 from "./web3";
 
 const instance = new web3.eth.Contract(
 	JSON.parse(JSON.stringify(discountmain.interface)), 
-	"0xD825fd815dC9ADC7D39b265c23f5CeEE8439BE84"
+	"0x822C5662cA0507FEA35E07F29210c55132e125A7"
 );
 
 export default instance;
--- ethereum/web3.js
+++ ethereum/web3.js
@@ -6,7 +6,7 @@ if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
 	web3 = new Web3(window.web3.currentProvider);
 } else {
 	const provider = new Web3.providers.HttpProvider(
-			"https://data-seed-prebsc-1-s1.binance.org:8545"
+			"https://data-seed-prebsc-2-s1.binance.org:8545"
 		);
 	web3 = new Web3(provider);
 }
